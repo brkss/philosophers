4905493153b6a7cc6a2833dac2de140da1475f8c
brkss <berkassebrahim@gmail.com>
2022-09-27 03:16:51 +0100

    🦀 lock mutex

--- philo/thread.c
+++ philo/thread.c
@@ -1,29 +1,41 @@
 
 
-
 #include <stdio.h>
 #include <pthread.h>
 
+typedef struct data_s {
+	int n;
+	pthread_mutex_t n_mtx;
+} data_t;
+
 void		*routine(void *arg)
 {
-	printf("print from thread ! : %d\n", *((int *)arg));
-	for(int i = 0; i < 10000000; i++);
-	*((int *)arg) += 1;
+	data_t *data = (data_t *)arg;
+	pthread_mutex_lock(&data->n_mtx);
+	printf("print from thread ! : %d\n", data->n);
+	for(int i = 0; i < 10000000; i++)
+		data->n += 1;
+	pthread_mutex_unlock(&data->n_mtx);
 }
 
 int main()
 {
-	int n;
+	data_t *data = (data_t *)malloc(sizeof(data_t));
+
+	data->n = 0;
+		
 	int i;
 
 	i = 0;
-	n = 0;
 	pthread_t	thread_id[2];
+
+	// init mutext !
+	pthread_mutex_init(&data->n_mtx, NULL);
 
 	// create threads !
 	while(i < 2)
 	{
-		pthread_create(&thread_id[i], NULL, &routine, &n);
+		pthread_create(&thread_id[i], NULL, &routine, data);
 		i++;
 	}
 	i = 0;
@@ -32,7 +44,8 @@ int main()
 		pthread_join(thread_id[i], NULL);
 		i++;
 	}
-	printf("after thread n = %d!\n", n);
+	pthread_mutex_destroy(&data->n_mtx);
+	printf("after thread n = %d!\n", data->n);
 	
 
 	return 0;
