33cd478bb34f9e4da6c6a3c6bc91dc70981903a8
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-09-29 00:45:50 +0100

    🧴 try semaphores

--- philo_bonus/philo.c
+++ philo_bonus/philo.c
@@ -2,11 +2,37 @@
 
 #include "includes/philo.h"
 
+# define NUM 4 
 
 int main()
 {
+  pid_t pid[NUM];
+  sem_t *sem;
+  int i;
 
-  printf("hello world !\n");
+  sem = sem_open("fork", O_CREAT, 0644, NUM);
+  
+  i = 0;
+  while(i < NUM)
+  {
+    pid[i] = fork();
+    if(pid[i] == 0)
+    {
+      sem_wait(sem);
+      sleep(1);
+      printf("this printed from the sup process\n");
+      sem_post(sem);
+      exit(0);
+    }
+    i++;
+  }
+
+  i = 0;
+  while(i < NUM)
+  {
+    waitpid(pid[i], NULL,0);
+    i++;
+  }
 
   return (0);
 }
